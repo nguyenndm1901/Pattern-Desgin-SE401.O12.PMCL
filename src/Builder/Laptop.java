c31521398df3de7335779ea6766e8e9bf8e3153b
Nguyên <nguyenndm1901@gmail.com>
2024-01-07 01:12:49 +0700

    first classes

---
+++ src/Builder/Laptop.java
@@ -0,0 +1,79 @@
+package Builder;
+
+public class Laptop {
+    private String code;
+    private String name;
+    private String brand;
+    private String processor;
+    private String memory;
+    private String storage;
+    private String unit;
+    private int price;
+
+    public Laptop() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
