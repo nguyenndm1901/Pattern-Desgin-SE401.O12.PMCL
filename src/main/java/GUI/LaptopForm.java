d4178c095efa631edcf0a2a48609fbad6705c64f
Nguyên <nguyenndm1901@gmail.com>
2024-01-09 02:29:29 +0700

    rename MainForm to LaptopForm

--- src/main/java/GUI/LaptopForm.java
+++ src/main/java/GUI/LaptopForm.java
@@ -34,12 +34,12 @@ import java.util.Set;
  *
  * @author Nguyen
  */
-public class MainForm extends javax.swing.JFrame {
+public class LaptopForm extends javax.swing.JFrame {
 
     /**
-     * Creates new form MainForm
+     * Creates new form LaptopForm
      */
-    public MainForm() {
+    public LaptopForm() {
         initComponents();
         initTable();
         initComboBox();
@@ -319,7 +319,6 @@ public class MainForm extends javax.swing.JFrame {
         btnAddOK.setText("OK");
         btnAddOK.setEnabled(false);
         btnAddOK.addActionListener(new java.awt.event.ActionListener() {
-            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnAddOKActionPerformed(evt);
             }
@@ -710,20 +709,20 @@ public class MainForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LaptopForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LaptopForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LaptopForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LaptopForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainForm().setVisible(true);
+                new LaptopForm().setVisible(true);
             }
         });
     }
